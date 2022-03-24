7bd2d9b0a523831b01b74c6af20d8293666bade9
MontayaM <89426842+MontayaM@users.noreply.github.com>
2022-03-23 22:16:38 -0500

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/central-native-checkpoint.py
+++
@@ -1,7 +0,0 @@
-def mean(list_of_nums):
-    total=0 
-    for num in list_of_numbers:
-        total=total+num
-        return total/len(list_of_numbers)
-    
-print(mean([47, 95,88, 73,88, 84]))  
